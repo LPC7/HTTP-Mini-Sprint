c475fafedc127be488eb6c11555097cc073aaeb6
manishavi <manishalal972@gmail.com>
2018-02-12 13:47:29 -0600

    displaying the friendlist

--- client/src/components/FriendsList.js
+++ client/src/components/FriendsList.js
@@ -1,25 +1,55 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 class FriendsList extends Component {
+  state = {
+    friends: [],
+    loading: false
+  };
+
+  // render() {
+  //   if(this.state.loading && this.state.noData) {
+  //     //will it run?
+  //   }
+  // }
+
   render() {
     return (
       <div>
         <div className="friend-title">Lambda Friends</div>
-        <ul className="friend-grid">
-          {this.state.friends.map(friend => {
-            return (
-              <li key={friend.id} className="friend">
-                <div className="friend-name">{friend.name}</div>
-                <div className="friend-age">{`Age: ${friend.age}`}</div>
-                <div className="friend-email">{`Email: ${friend.email}`}</div>
-              </li>
-            );
-          })}
-        </ul>
+        {this.state.loading && <div>Loading Friends...</div>}
+
+        {!this.state.loading && (
+          <ul className="friend-grid">
+            {this.state.friends.map(friend => {
+              return (
+                <li key={friend.id} className="friend">
+                  <div className="friend-name">{friend.name}</div>
+                  <div className="friend-age">{`Age: ${friend.age}`}</div>
+                  <div className="friend-email">{`Email: ${friend.email}`}</div>
+                </li>
+              );
+            })}
+          </ul>
+        )}
       </div>
     );
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true });
+    axios
+      .get("http://localhost:5000/friends")
+      .then(response => {
+        this.setState({ friends: response.data, loading: false });
+      })
+      .catch(error => {
+        this.setState({ loading: false });
+        console.log("there was error", error);
+      });
   }
 }
 
 export default FriendsList;
+
+// http://localhost:5000/friends
